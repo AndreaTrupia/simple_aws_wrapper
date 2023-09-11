cabdec01985a47c16ff165409dba9e6e5aca8c21
Andrea Trupia <a.trupia@innonation.it>
2023-09-11 16:26:19 +0200

    [0.1.3] add support for table scan in dynamodb

--- src/simple_aws_wrapper/AWS.py
+++ src/simple_aws_wrapper/AWS.py
@@ -1,5 +1,7 @@
 from __future__ import annotations
 
+from typing import List
+
 import boto3
 
 
@@ -7,6 +9,7 @@ class GenericException(Exception):
     """
     Eccezione generica
     """
+
     ...
 
 
@@ -16,7 +19,9 @@ class AWS:
     """
 
     @staticmethod
-    def get_client(service_name: str, region_name: str, endpoint_url: str | None = None):
+    def get_client(
+        service_name: str, region_name: str, endpoint_url: str | None = None
+    ):
         """
         Funzione per instaurare una sessione Boto3. Restituisce il session client relativo al servizio
         :param service_name: servizio con cui instaurare una connessione (es. "s3" o "dynamodb")
@@ -26,11 +31,15 @@ class AWS:
         """
         session = boto3.Session()
         if endpoint_url:
-            return session.client(service_name, region_name=region_name, endpoint_url=endpoint_url)
+            return session.client(
+                service_name, region_name=region_name, endpoint_url=endpoint_url
+            )
         return session.client(service_name, region_name=region_name)
 
     @staticmethod
-    def get_resource(service_name: str, region_name: str, endpoint_url: str | None = None):
+    def get_resource(
+        service_name: str, region_name: str, endpoint_url: str | None = None
+    ):
         """
         Funzione per prendere una risorsa aws
         :param service_name: nome servizio (ad esempio "dynamodb")
@@ -68,10 +77,31 @@ class S3:
             print(str(e))
             raise GenericException
 
+
 class DynamoDB:
     """
     Classe per la gestione di DynamoDB su AWS
     """
+
+    def __get_table_resource(self, table_name: str):
+        """
+        Funzione per effettuare la get di una risorsa di tipo tabella di DynamoDB
+        :param table_name: nome tabella
+        :return: dynamodb.Table
+        """
+        try:
+            if self.endpoint_url and self.endpoint_url != "":
+                dynamodb_table = AWS.get_resource(
+                    "dynamodb", self.region_name, self.endpoint_url
+                ).Table(table_name)
+            else:
+                dynamodb_table = AWS.get_resource("dynamodb", self.region_name).Table(
+                    table_name
+                )
+            return dynamodb_table
+        except Exception as e:
+            print(str(e))
+            raise GenericException
 
     def __init__(self, region_name: str, endpoint_url: str | None = None):
         self.region_name = region_name
@@ -85,12 +115,29 @@ class DynamoDB:
         :return: None
         """
         if self.endpoint_url and self.endpoint_url != "":
-            dynamodb_table = AWS.get_resource("dynamodb", self.region_name, self.endpoint_url).Table(table_name)
+            dynamodb_table = AWS.get_resource(
+                "dynamodb", self.region_name, self.endpoint_url
+            ).Table(table_name)
         else:
-            dynamodb_table = AWS.get_resource("dynamodb", self.region_name).Table(table_name)
+            dynamodb_table = AWS.get_resource("dynamodb", self.region_name).Table(
+                table_name
+            )
         try:
             dynamodb_table.put_item(Item=item)
             return True
+        except Exception as e:
+            print(str(e))
+            raise GenericException
+
+    def scan_table(self, table_name: str) -> List[dict]:
+        """
+        Funzione per prelevare tutti gli elementi all'interno di una tabella
+        :param table_name: nome tabella
+        :return: lista in cui ogni elemento è un dizionario le cui chiavi sono i campi del db e i valori sono i
+        rispettivi valori
+        """
+        try:
+            return self.__get_table_resource(table_name).scan()
         except Exception as e:
             print(str(e))
             raise GenericException
@@ -142,6 +189,7 @@ class SQS:
             print(str(e))
             raise GenericException
 
+
 class ParameterStore:
     """
     Classe per la gestione del servizio ParameterStore di AWS
@@ -162,11 +210,13 @@ class ParameterStore:
         ssm = AWS.get_client("ssm", self.region_name, self.endpoint_url)
         for parameter in parameters_list:
             try:
-                output_dict[parameter] = ssm.get_parameters(Names=[parameter], WithDecryption=True)["Parameters"][0][
-                    "Value"
-                ]
+                output_dict[parameter] = ssm.get_parameters(
+                    Names=[parameter], WithDecryption=True
+                )["Parameters"][0]["Value"]
             except:
-                print(f"Errore nel recupero della chiave {parameter} dal parameter store")
+                print(
+                    f"Errore nel recupero della chiave {parameter} dal parameter store"
+                )
                 output_dict[parameter] = ""
                 raise GenericException
         return output_dict
