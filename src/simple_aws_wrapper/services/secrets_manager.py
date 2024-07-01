6deaaf0ffb4e358bcacb28a957870866772dda44
Andrea Trupia <a.trupia@innonation.it>
2024-07-01 11:08:02 +0200

    [2.9.0] AWS Secrets Manager: create_secret; delete_secret; list_secret

---
+++ src/simple_aws_wrapper/services/secrets_manager.py
@@ -0,0 +1,111 @@
+from __future__ import annotations
+
+from simple_aws_wrapper.config import AWSConfig
+from simple_aws_wrapper.const import services
+from simple_aws_wrapper.exceptions.exceptions import MissingConfigurationException, GenericException
+from simple_aws_wrapper.resource_manager import ResourceManager
+
+import traceback
+
+
+class SecretsManager:
+    def __init__(self):
+        if not AWSConfig().is_configured():
+            raise MissingConfigurationException
+        self.client = ResourceManager.get_client(
+            services.SECRETS_MANAGER, **AWSConfig().to_dict()
+        )
+        self.region_name = AWSConfig().get_region_name()
+
+    def create_secret(
+        self,
+        name: str,
+        secret_string: str,
+        **kwargs
+    ) -> bool:
+        """
+        Crea un secret
+        :param name: nome del secret
+        :param secret_string: stringa del secret
+        :param tags: tag per il secret
+        :return: dict del secret creato
+        """
+        try:
+            self.client.create_secret(Name=name, SecretString=secret_string, **kwargs)
+            return True
+        except Exception:
+            raise GenericException(traceback.format_exc())
+
+    def get_secret_value(
+        self,
+        secret_id: str,
+        **kwargs
+    ) -> dict:
+        """
+        Recupera un secret
+        :param secret_id: id del secret
+        :return: dict del secret
+        """
+        return self.client.get_secret_value(SecretId=secret_id, **kwargs)["SecretString"]
+
+    def get_secret_id_by_name(
+        self,
+        name: str,
+        **kwargs
+    ) -> str:
+        """
+        Trova l'id del secret by name
+        :param name: nome del secret
+        :return: id del secret
+        """
+        try:
+            return self.client.get_secret_value(SecretId=name, **kwargs)["ARN"]
+        except Exception:
+            raise GenericException(traceback.format_exc())
+
+    def delete_secret(
+        self,
+        secret_id: str,
+        **kwargs
+    ) -> bool:
+        """
+        Elimina un secret
+        :param secret_id: id del secret
+        :return: True se la creazione Ã© andata bene
+        """
+        try:
+            self.client.delete_secret(SecretId=secret_id, **kwargs)
+            return True
+        except Exception:
+            raise GenericException(traceback.format_exc())
+
+    def list_secrets(
+        self,
+        **kwargs
+    ) -> list:
+        """
+        Lista tutti i secrets. Ogni elemento contiene un dizionario con le seguenti chiavi:
+            - ARN (Amazon Resource Name) del secret
+            - Name (nome) del secret
+            - CreatedDate (data di creazione del secret)
+            - SecretVersionsToStages (mappa delle versioni del secret a stage)
+            - LastChangedDate (data dell'ultima modifica del secret)
+            - LastAccessedDate (data dell'ultima accesso al secret)
+        :param kwargs: parametri aggiuntivi
+        :return: lista dei secrets
+        """
+        try:
+            return self.client.list_secrets(**kwargs)["SecretList"]
+        except Exception:
+            raise GenericException(traceback.format_exc())
+
+    def get_secret_name_by_id(self, secret_id: str) -> str:
+        """
+        Trova il nome del secret by id
+        :param secret_id: id del secret
+        :return: nome del secret
+        """
+        try:
+            return self.client.describe_secret(SecretId=secret_id)["Name"]
+        except Exception:
+            raise GenericException(traceback.format_exc())
---
+++ tests/test_secrets_manager.py
@@ -0,0 +1,41 @@
+import random
+import string
+import unittest
+
+from simple_aws_wrapper.config import AWSConfig
+from simple_aws_wrapper.const import regions
+from simple_aws_wrapper.const.regions import Region
+from simple_aws_wrapper.services.secrets_manager import SecretsManager
+
+
+class TestSecretsManager(unittest.TestCase):
+    @staticmethod
+    def generate_1mb_string():
+        size_in_bytes = 1 * 1024 * 1024
+        characters = string.ascii_letters + string.digits
+        result_string = ''.join(random.choices(characters, k=size_in_bytes))
+        return result_string
+
+    def setUp(self) -> None:
+        AWSConfig().set_region(Region(regions.EU_WEST_1)).set_endpoint_url(
+            "http://localhost:4566"
+        ).set_aws_secret_access_key("test").set_aws_access_key_id(
+            "test"
+        ).set_aws_session_token(
+            "test"
+        )
+        self.secrets_manager = SecretsManager()
+        self.secret_name = f"test-secret-{random.randint(0, 1000)}"
+        self.secret_string = self.generate_1mb_string()
+
+    def test_create_secret(self):
+        self.secrets_manager.create_secret(
+            name=self.secret_name,
+            secret_string=self.secret_string,
+        )
+        secret_id = self.secrets_manager.get_secret_id_by_name(self.secret_name)
+        secret_value = self.secrets_manager.get_secret_value(secret_id=secret_id)
+        self.assertEqual(secret_value, self.secret_string)
+        self.assertTrue(self.secrets_manager.get_secret_name_by_id(secret_id)
+                        in [x["Name"] for x in self.secrets_manager.list_secrets()])
+        self.assertTrue(self.secrets_manager.delete_secret(secret_id=secret_id))
